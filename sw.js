c076964962c64225ef16817b6f933a317ff3f2c1
arqaz <joaovitorpereiraazevedo@gmail.com>
2026-08-13 21:32:46 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+self.addEventListener('install', (e) => { self.skipWaiting(); });
+self.addEventListener('activate', (e) => { self.clients.claim(); });
+self.addEventListener('fetch', (e) => {
+  // passa tudo diretamente para a rede; sem cache offline por agora
+});
